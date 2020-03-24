20e63dce2f8d51dc9f8d1bf255f3bb012d990991
nilipta <niliptasatapathy@gmail.com>
2020-03-24 17:15:59 +0530

    added table with colcor...

--- main.py
+++ main.py
@@ -7,53 +7,11 @@ import PySimpleGUI as sg
 
 # sg.theme('Dark Red')
 # sg.theme('Default1')
-# sg.set_options(text_color='black', background_color='#A6B2BE', text_element_background_color='#A6B2BE')
-# ------ Menu Definition ------ #
-menu_def = [['&File', ['&Open', '&Save', 'E&xit', 'Properties']],
-            ['&Edit', ['Paste', ['Special', 'Normal', ], 'Undo'], ],
-            ['&Help', '&About...'], ]
-
-# ------ Column Definition ------ #
-column1 = [[sg.Text('Column 1', justification='center', size=(10, 1))],
-           [sg.Spin(values=('Spin Box 1', '2', '3'),
-                    initial_value='Spin Box 1')],
-           [sg.Spin(values=('Spin Box 1', '2', '3'),
-                    initial_value='Spin Box 2')],
-           [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 3')]]
-
-layoutX = [
-    [sg.Menu(menu_def, tearoff=True)],
-    [sg.Text('(Almost) All widgets in one Window!', size=(
-        30, 1), justification='center', font=("Helvetica", 25), relief=sg.RELIEF_RIDGE)],
-    [sg.Text('Here is some text.... and a place to enter text')],
-    [sg.InputText('This is my text')],
-    [sg.Frame(layout=[
-        [sg.CBox('Checkbox', size=(10, 1)),
-         sg.CBox('My second checkbox!', default=True)],
-        [sg.Radio('My first Radio!     ', "RADIO1", default=True, size=(10, 1)),
-         sg.Radio('My second Radio!', "RADIO1")]], title='Options', relief=sg.RELIEF_SUNKEN, tooltip='Use these to set flags')],
-    [sg.MLine(default_text='This is the default Text should you decide not to type anything', size=(35, 3)),
-     sg.MLine(default_text='A second multi-line', size=(35, 3))],
-    [sg.Combo(('Combobox 1', 'Combobox 2'),default_value='Combobox 1', size=(20, 1)),
-     sg.Slider(range=(1, 100), orientation='h', size=(34, 20), default_value=85)],
-    [sg.OptionMenu(('Menu Option 1', 'Menu Option 2', 'Menu Option 3'))],
-    [sg.Listbox(values=('Listbox 1', 'Listbox 2', 'Listbox 3'), size=(30, 3)),
-     sg.Frame('Labelled Group', [[
-         sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=25, tick_interval=25),
-         sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=75),
-         sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=10),
-         sg.Col(column1)]])
-    ],
-    [sg.Text('_' * 80)],
-    [sg.Text('Choose A Folder', size=(35, 1))],
-    [sg.Button('Set'), sg.Text('Your Folder', size=(15, 1), justification='right'),
-     sg.InputText('Default Folder'), sg.FolderBrowse()],
-    [sg.Submit(tooltip='Click to submit this form'), sg.Cancel()]]
 
 layout = [
     [sg.Text('VAS store', size=(100, 1), pad=(0, (30,0)), justification='center', font=("Helvetica", 25))],
      [sg.T()],
-    [sg.Button('Search' , size=(20, 2), pad=((100,20),0)), sg.InputText('Default Folder', size=(80, 3))],
+    [sg.Button('Search' , size=(20, 2), pad=((100,20),0)), sg.Input("location/part No.",do_not_clear=False, size=(80, 10))],
      [sg.T()],
      [sg.T()],
      [sg.T()],
@@ -90,23 +48,26 @@ layout3 = [
     [sg.Cancel('Exit', pad=((650,0),(60,0)))]
     ]
 
+
+# sg.theme('DarkBlue12')
 MAX_ROWS = 10
 MAX_COL = 5
 headings = ['Location', 'Part No.', 'No Of Boxes','Last Update Date', 'Last Update By']
 # header =  [[sg.Text('  ')] + [sg.Text(h, size=(14,1)) for h in headings]]
-columm_layout =  [[sg.Text(str(i), size=(4, 1), justification='left')] + [sg.Input(size=(18, 1), pad=(
-        1, 1), justification='right', key=(i, j)) for j in range(MAX_COL)] for i in range(1, MAX_ROWS+1)]
+different_column_size=[20,10,12,25,25]
+different_column_size1=[15,7,12,20,25]
+columm_layout =  [[sg.Text(str(i), size=(4, 1), justification='left')] + [sg.Input(size=(different_column_size[j], 1), pad=(
+        1, 1), justification='right', key=(i, j), background_color='lightskyblue') for j in range(MAX_COL)] for i in range(1, MAX_ROWS+1)]
 
 layout4 = [
             [sg.Text('VAS store', size=(100, 1), pad=(0, (30,0)), justification='center', font=("Helvetica", 25))],
             [sg.Text('Stock Status', pad=((50,0),(0,0)) )],
-            [sg.Text(size=(10, 1))]+ [sg.Text(h, size=(15,1), pad=((0,17),(0,0)) ) for h in headings],
+            [sg.Text(size=(10, 1))]+ [sg.Text(h, size=(different_column_size1[headings.index(h)], 1), pad=((0,17),(0,0)) ) for h in headings],
             [sg.Col(columm_layout, size=(750, 300), scrollable=False, pad=(0,0))],
             [sg.Submit('Export To Excel', pad=((380,0),(0,0)) ), sg.Cancel('Exit', pad=((10,0),(0,0)) )],
         ]
 #Stock Status
 # layout4 = table_main_heading + heading_status + header + input_rows
-
 
 windowNo = 0
 # print(sg.Window.get_screen_size())
@@ -114,6 +75,9 @@ window = sg.Window('VAS store', layout, size=(800, 480), no_titlebar=False, auto
 windowNo=1
 window2 = sg.Window('VAS store', layout2, size=(800, 480), no_titlebar=True, auto_size_buttons=True, location=(0,50), resizable=False)
 window3 = sg.Window('VAS store', layout3, size=(800, 480), no_titlebar=True, auto_size_buttons=True, location=(0,50), resizable=False)
+
+
+
 window4 = sg.Window('VAS store', layout4, size=(800, 480), no_titlebar=True, auto_size_buttons=True, location=(0,50), resizable=False)
 # window.Maximize()
 
@@ -153,6 +117,7 @@ while True:             # Event Loop
         window.refresh()
         window3.refresh()
     if event == 'Stock Status':
+        # sg.theme('Dark Brown 2')
         window4.UnHide()
         # print('Withdraw sock')
         windowNo=4
